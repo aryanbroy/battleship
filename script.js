052dc46254b620b36d508cae019902522ea84843
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2023-11-18 01:49:52 +0530

    find the login behind placing a ship

--- script.js
+++ script.js
@@ -1,69 +1,84 @@
-let ships =  {
-        carrier : {
-            'length': 5,
-            'hit': 0
-        },
-        battleship : {
-            'length': 4,
-            'hit': 0
-        },
-        cruiser : {
-            'length': 3,
-            'hit': 0
-        },
-        submarine : {
-            'length': 3,
-            'hit': 0
-        },
-        destroyer : {
-            'length': 2,
-            'hit' : 0
-        }
-};
+const shipsDiv = document.querySelector(".ships");
+const gameBoard = document.querySelector(".grid-container");
+
+const gameArray = Array.from(gameBoard.children);
+let i = 1;
+gameArray.forEach((item) => {
+    item.id = i;
+    i++;
+})
+
+const shipsArray = Array.from(shipsDiv.children)
 
 
-let ship = (length) => {
-    let shipLength = length;
-    let hitCount = 0;
-
-    let hit = () => {
-        hitCount++;
+class ship{
+    constructor(name, length) {
+        this.name = name;
+        this.length = length;
     }
-
-    let isSunk = () => {
-        if (hitCount === shipLength) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    return {shipLength, hitCount ,hit, isSunk}
 }
 
-let placeDiv = (x_pos, y_pos) => {
-    var d = document.getElementById('carrier');
-    d.style.position = "absolute";
-    d.style.left = x_pos+'px';
-    d.style.top = y_pos+'px';
-}
+const carrier = new ship('carrier', 5);
+const battleship = new ship('battleship', 4);
+const cruiser = new ship('cruiser', 3);
+const submariner = new ship('submarine', 3);
+const destroyer = new ship('destroyer', 2);
 
-placeDiv(1, 83);
+//helps in placing the ship
+// let placeDiv = (x_pos, y_pos) => {
+//     var d = document.getElementById('carrier');
+//     d.style.position = "absolute";
+//     d.style.left = x_pos+'px';
+//     d.style.top = y_pos+'px';
+// }
+// placeDiv(0+1, 0 + 1)
+// placeDiv(40 + 2, 40 + 2);
+// placeDiv(80+3, 80 + 3);
+
+
 
 var gridItems = document.querySelectorAll('.grid-item');
 
     gridItems.forEach(function(item) {
-        item.addEventListener('click', function() {
-            getPosition(item);
+        item.addEventListener('click', function(e) {
+            e.target.classList.add('selected')
+            // placeDiv(1, 83);
+            let column = getPosition(e.target).column;
+            let row = getPosition(e.target).row;
+            console.log(column);
+            console.log(row);
         });
     });
 
-    function getPosition(element) {
-        var index = Array.from(element.parentElement.children).indexOf(element);
+function getPosition(element) {
+    var index = Array.from(element.parentElement.children).indexOf(element);
         
-        var column = index % 3 + 1; // Assuming 3 columns
-        var row = Math.floor(index / 3) + 1;
+    var column = index % 3 + 1; // Assuming 3 columns
+    var row = Math.floor(index / 3) + 1;
 
-        alert("Row: " + row + ", Column: " + column);
+    return { column, row };
+}
+
+function addShip(length) {
+    let randomIndex = Math.floor(Math.random() * 100) + 1;
+    let tf = [true, false];
+    let randomBoolean = Math.floor(Math.random() * 2);
+    let idArr = [];
+    let horizontal = tf[randomBoolean];
+    console.log(horizontal)
+    for (let i = 0; i < length; i++){
+        if (horizontal === true) {
+            const findId = document.getElementById(`${randomIndex + i}`);
+            findId.classList.add('selected')
+            idArr.push(findId)
+        }
+        else {
+            const findId = document.getElementById(`${randomIndex + (i * 10)}`);
+            findId.classList.add('selected')
+            idArr.push(findId);
+        }
     }
+    console.log(idArr)
+}
 
+addShip(destroyer.length);
